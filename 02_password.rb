8dea2d466a2e4feb8eadc809be586827aa9bf90c
MMartin <91570588+MartinMarcDWARF@users.noreply.github.com>
2021-10-06 22:10:48 +0200

    fini manque que l'ascenceur

--- 02_password.rb
+++ 02_password.rb
@@ -1,27 +1,40 @@
 def sign_up
-  puts "Please create a password in order to sign up"
+  puts "| Please create a password in order to sign up |"
   print ">"
   return user_password = gets.chomp
 end
   
 def login(pwd)
-  puts "Insert your password"
+  puts "                    "
+  puts "| Insert your password |"
   print ">"
   user_login = gets.chomp
   while user_login != pwd
-    puts "Incorrect Password","Insert your password"
+    puts "                         "
+    puts "! Incorrect Password !\n", "             ", "| Insert your password |"
+    print ">"
     user_login = gets.chomp
+    puts "                        "
   end  
-    puts "Password correct ! Please wait"
+  puts "                          "  
+  puts "| Password correct ! Please wait |"
 end
   
   def atta 
-    10.times do
+    puts "                        "
+    4.times do
   for i in ". ".chars.to_a
     print i
-    sleep 0.5
-  end
-  end
+    sleep 0.8
+  end  
+end
+end
+
+def wlc_screen
+  puts "                                                                                                                                                      "
+  puts " _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ /Geilt-H4CKN3T-V0.4.2-/_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n\n                                    Vilkommen Geilt !\n\n\n     This is a secret-Defense list : Not to share ! Reader's discretion required\n\n     - Lucas made several big orthograph mistakes\n     - Daenerys died in Season 8 Final\n     - Vader rules\n     - Pierre is a total bg\n     - Jedis sucks, Siths are way better. Hail the galactic empire !\n     - Alex has some real big weed cooking skills\n     - No matter what, no one can hear you scream in space.\n     - Ruby is killing me\n     - Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn\n     - Tackle tickles the tickling tickles\n\n          Transmission is over, device shall self-destruct. Pay respect to Vocal-01 !\n _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ "
+  sleep 2
+
 end
 
 
@@ -36,6 +49,7 @@ end
     mdp = sign_up
     login(mdp)
     atta
+    wlc_screen
   end
 
   perform
---
+++ 03_stairway.rb
@@ -0,0 +1,9 @@
+def saisis
+  puts "Appuie sur Entrée" 
+  var = gets.chomp.to_i
+  
+  
+  def random(var) 
+    rand(6)+1
+  return chiffre
+  end
